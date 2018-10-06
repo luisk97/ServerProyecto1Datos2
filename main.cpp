acec172b386ea140f180d351c2f52fd8a4d4fc7f
luisk97 <30986170+luisk97@users.noreply.github.com>
2018-10-06 02:02:16 -0600

    Version final del Servidor

--- main.cpp
+++ main.cpp
@@ -18,6 +18,161 @@
 
 using namespace std;
 
+struct Node{
+    int posicion;
+    int referencias = 0;
+    int id;
+    int idMax;
+    int size;
+    int tamMax;
+    struct Node *next;
+};
+
+typedef struct Node *SimpleList;
+
+template<typename T>
+void mostrarLista(SimpleList &list) {
+    int i = 0;
+    if(list == NULL) {
+        cout << "Lista Vacia!!"<<endl;
+    }else{
+        while (list != NULL) {
+            cout << ' ' << i + 1 << ") " << "id:" << list->id << " , direccion guarda:" << list->posicion
+                 << " , referencias:" << list->referencias << endl;
+            list = list->next;
+            i++;
+        }
+    }
+}
+
+template <typename T>
+int deleteSL(SimpleList &list, int id) {
+    SimpleList l1,l2,l3;
+    l1 = list;
+    int posElim;
+
+    if(list!=NULL) {
+        while(l1!=NULL) {
+            if(l1->id == id) {
+                if (l1->referencias == 1) {
+                    posElim = l1->posicion;
+                    l3 = l1->next;
+                    while(l3 == NULL){
+                        l3->posicion--;
+                        l3 = l3->next;
+                    }
+                    //cout<<"OKY"<<endl;
+                    if (l1 == list) {
+                        (list->next)->size = (list->size)-1;
+                        (list->next)->idMax = (list->idMax);
+                        list = list->next;
+                    }else{
+                        l2->next = l1->next;
+                    }
+                    return posElim;
+
+                }else{
+                    //cout<<"OKYDOKY"<<endl;
+                    l1->referencias--;
+                    return -1;
+                }
+
+            }
+            l2 = l1;
+            l1 = l1->next;
+        }
+        cout<<"El dato que se desea eliminar no existe"<<endl;
+        return -2;
+    } else {
+        cout << " Lista vacia..!";
+        return -2;
+    }
+}
+
+template<typename T>
+int addEspacio(SimpleList &list, int tamMaximo){
+    SimpleList l1, l2 = new(struct Node);
+
+    l2->tamMax = tamMaximo;
+    l2->next = NULL;
+
+    if(list==NULL){
+        list = l2;
+        list->id = 1;
+        list->referencias=1;
+        list->idMax = 1;
+        list->size++;
+        list->posicion = 0;
+        return 1;
+    }
+    else{
+        int indice = 2;
+        l1 = list;
+        while(l1->next!=NULL){
+            l1 = l1->next;
+            indice++;
+        }
+        if(l1->tamMax > indice) {
+            //cout<<"Si entramos3"<<endl;
+            l2->referencias++;
+            l1->next = l2;
+            l2->posicion = (l1->posicion)+1;
+            list->idMax++;
+            l2->id = list->idMax;
+            list->size++;
+            return l2->id;
+        }else{
+            //cout<<"Si entramos4"<<endl;
+            return 0;
+        }
+
+    }
+}
+
+template<typename T>
+void amentarReferencia(SimpleList &list, int id){
+
+    SimpleList l1 = new(struct Node);
+
+    if(list==NULL){
+        cout<<"lista vacia"<<endl;
+    }
+    else{
+        l1 = list;
+        while(l1!=NULL){
+            if(l1->id == id){
+                //cout<<"Si entramos"<<endl;
+                l1->referencias++;
+                return;
+            }else {
+                l1 = l1->next;
+            }
+        }
+        cout<<"No existe la posicion a la que se le desea aumentar referencias"<<endl;
+    }
+}
+
+template <class T>
+int searchDato(SimpleList list, int id) {
+    SimpleList l1 = list;
+
+    while(l1!=NULL) {
+        if(l1->id == id) {
+            return l1->posicion;
+        }else {
+            l1 = l1->next;
+        }
+    }
+    return -1;
+}
+
+
+
+//--------------------------------------------------------------------
+
+
+
+
 struct dataSocket{
     int descriptor;
     sockaddr_in info;
@@ -73,7 +228,7 @@ public:
             //Aqui se debe llamar a las funcion que busca el dato mediante
             //el id (numId) que se recibio del cliente
 
-            int dato = 104;//Aqui debe ir el dato que se encuentre en el id solicitado
+            int dato = pedirDato(numId);//Aqui debe ir el dato que se encuentre en el id solicitado
             Json::Value root;
             string strID = std::to_string(dato);
             string text = "{ \"Dato\": " + strID + "}";
@@ -96,12 +251,18 @@ public:
 
             //Aqui es donde se debe llamar a la funcion que hace el malloc y
             //lo hacemos del tamaño de numTam
+            bool exito;
+
+            exito = reservarMemoria(numTam);
 
             string text;
             //verificamos y si se pudo recerbar la memoria respondemos
-            text = "Exito";
-            //Y si no respondemos
-            text = "Error";
+            if(exito) {
+                text = "Exito";
+            }else {
+                //Y si no respondemos
+                text = "Error";
+            }
 
             cout <<"Respondiendo con el mensaje: "<< text << endl;
 
@@ -122,7 +283,7 @@ public:
             //la memoria reservada y lo agregue al mapa de memoria y reserve la
             //memoria y nos devuelva el id donde quedo guardado y se lo asigne
             //a la siguiente variable
-            int id = 3;//si no se pudo reservar la memoria para el MPointer entonces
+            int id = reservarEspacio();//si no se pudo reservar la memoria para el MPointer entonces
             //devuelve cero y si se pudo devuelve los id de 1 en adelante
 
 
@@ -148,17 +309,22 @@ public:
             int numId = atoi(strId.c_str());//con esta variable buscamos el espacio a actualizar
 
             string strDato = json["dato"].asString();
-            int numDato = atoi(strId.c_str());//reemplazamos el dato que se encuentra
+            int numDato = atoi(strDato.c_str());//reemplazamos el dato que se encuentra
             //en el id buscado por numDato
+
+            bool exito = actualizarDato(numDato,numId);
 
             //Aqui se debe llamar a las funcion que recibe como parametro el id y el dato
             // busca el campo mediante el id (numId) y lo actualiza con numDato
 
             string text;
-            //verificamos y si se pudo actualizar el dato respondemos
-            text = "Exito";
-            //Y si no respondemos
-            text = "Error";
+            if(exito) {
+                //verificamos y si se pudo actualizar el dato respondemos
+                text = "Exito";
+            }else {
+                //Y si no respondemos
+                text = "Error";
+            }
 
             cout <<"Respondiendo con el mensaje: "<< text << endl;
 
@@ -170,18 +336,19 @@ public:
             string strIdElim = json["idElim"].asString();
             int numIdElim = atoi(strIdElim.c_str());//con esta variable buscamos el espacio a liberar
 
-            //Aqui se debe llamar a las funcion que recibe como parametro el id
-            //busca el campo mediante el id (numIdElim) y disminuye en uno las referencias
-            //hacia esta y si las referencias es igual a cero libera ese espacio
-
             string strIdAum = json["idElim"].asString();
             int numIdAum = atoi(strIdAum.c_str());//y con esta aumentamos las referencias a este id
 
+            bool exito = liberarEspacio(numIdElim,numIdAum);
+
             string text;
-            //verificamos y si la accion se realizo correctamente respondemos
-            text = "Exito";
-            //Y si no respondemos
-            text = "Error";
+            if(exito) {
+                //verificamos y si la accion se realizo correctamente respondemos
+                text = "Exito";
+            }else {
+                //Y si no respondemos
+                text = "Error";
+            }
 
             cout <<"Respondiendo con el mensaje: "<< text << endl;
 
@@ -194,11 +361,16 @@ public:
             //Aqui es donde se debe llamar a la funcion que hace el malloc y
             //lo hacemos del tamaño de numTam
 
+            bool exito = liberarMemoria();
+
             string text;
-            //verificamos y si se pudo recerbar la memoria respondemos
-            text = "Exito";
-            //Y si no respondemos
-            text = "Error";
+            if(exito) {
+                //verificamos y si se pudo recerbar la memoria respondemos
+                text = "Exito";
+            }else {
+                //Y si no respondemos
+                text = "Error";
+            }
 
             cout <<"Respondiendo con el mensaje: "<< text << endl;
 
@@ -223,7 +395,75 @@ private:
     int descriptor;
     sockaddr_in info;
     vector<int> clientes;
-    //Aqui entre estas variables puede ir tambien el puntero hacia el malloc que se haga
+    //-----------------------------------------------------------------
+    int* ptr = NULL;
+    SimpleList list = NULL;
+    int tamMax = 0;
+
+    bool reservarMemoria(int tam){
+        if(ptr == NULL){
+            ptr = (int*)malloc(tam);
+            tamMax = (tam/4);
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    bool liberarMemoria(){
+        if(ptr != NULL){
+            free(ptr);
+            ptr = NULL;
+            list = NULL;
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    int reservarEspacio(){
+        int id = addEspacio<int>(list,tamMax);
+        return id;
+        //si id = 0 entoncs no hay memoria
+    }
+
+    bool actualizarDato(int dato, int id){
+        int pos = searchDato<int>(list, id);
+        if(pos == -1){
+            return false;
+        }else{
+            ptr[pos] = dato;
+            return true;
+        }
+    }
+
+    int pedirDato(int id){
+        int pos = searchDato<int>(list,id);
+        if(pos == -1){
+            return NULL;
+        }else{
+            return ptr[pos];
+        }
+    }
+
+    bool liberarEspacio(int idElim, int idAum){
+        amentarReferencia<int>(list,idAum);
+        int posicionElim = deleteSL<int>(list,idElim);
+        if(posicionElim == -1){
+            return true;
+        }else if(posicionElim == -2){
+            return false;
+        }else{
+            int maximo = list->size;
+            while(posicionElim < (maximo-1)){
+                ptr[posicionElim] = ptr[posicionElim+1];
+                posicionElim++;
+            }
+            return true;
+        }
+    }
+
+    //-----------------------------------------------------------------
 
     bool crearSocket(){
         descriptor = socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
@@ -293,20 +533,6 @@ void * serverRun(void *){
 int main(int argc, char *argv[]) {
     server = new SocketServer;
     serverRun(NULL);
-
-    /*
-    pthread_t hiloServer;
-    pthread_create(&hiloServer,0,serverRun,NULL);
-    pthread_detach(hiloServer);
-    */
-
-    /*
-    while(1){
-        string mensaje;
-        cin>> mensaje;
-        server->setMensaje(mensaje.c_str());
-    }
-    */
 
     delete server;
 
